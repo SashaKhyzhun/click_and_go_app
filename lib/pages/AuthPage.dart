335b0b0ddafccbde8140ae894b1f440f6a7dda08
SashaKhyzhun <sasha.khyzhun@gmail.com>
2020-05-05 23:40:31 +0300

    auth;

---
+++ lib/pages/AuthPage.dart
@@ -0,0 +1,126 @@
+import 'package:clickandgoapp/controllers/MainBottonNavController.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AuthPage extends StatefulWidget {
+  @override
+  _AuthPageState createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: true,
+      body: Container(
+
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 200,
+              height: 100,
+              margin: EdgeInsets.only(bottom: 64),
+              child: Image(
+                image: AssetImage("assets/splash_icon.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 32, right: 32),
+              child: createEmailEditText(context),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 32, right: 32),
+              child: createDescriptionEditText(context),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 32),
+              child: RaisedButton(
+                color: Colors.teal,
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) =>
+                      HomeBottomNavigationController()));
+                },
+                child: Container(
+                  child: Text("Войти в систему", style: TextStyle(color: Colors.white),),
+                ),
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
+
+Widget createEmailEditText(
+    //TextEditingController emailController,
+    BuildContext context) {
+  return Container(
+    child: TextField(
+      //controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      textInputAction: TextInputAction.done,
+      onSubmitted: (_) => {FocusScope.of(context).unfocus()},
+      decoration: InputDecoration(
+        hintText: 'Введите почту',
+        hintStyle: TextStyle(fontSize: 16.0, color: Colors.grey),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.black),
+        ),
+      ),
+      style: TextStyle(
+        color: Colors.black,
+        fontStyle: FontStyle.normal,
+        fontSize: 16.0,
+        fontWeight: FontWeight.normal,
+      ),
+      maxLines: 1,
+      textCapitalization: TextCapitalization.none,
+      textAlign: TextAlign.center,
+      cursorColor: Colors.grey,
+      enableSuggestions: false,
+    ),
+  );
+}
+
+Widget createDescriptionEditText(
+    //TextEditingController emailController,
+    BuildContext context) {
+  return TextField(
+    //controller: emailController,
+    keyboardType: TextInputType.visiblePassword,
+    textInputAction: TextInputAction.done,
+    onSubmitted: (_) => {FocusScope.of(context).unfocus()},
+    decoration: InputDecoration(
+      hintText: 'Введите пароль',
+      hintStyle: TextStyle(fontSize: 16.0, color: Colors.grey),
+      enabledBorder: UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.grey),
+      ),
+      focusedBorder: UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.black),
+      ),
+    ),
+    style: TextStyle(
+      color: Colors.black,
+      fontStyle: FontStyle.normal,
+      fontSize: 16.0,
+      fontWeight: FontWeight.normal,
+    ),
+    maxLines: 1,
+    obscureText: true,
+    textCapitalization: TextCapitalization.none,
+    textAlign: TextAlign.center,
+    cursorColor: Colors.grey,
+    enableSuggestions: false,
+  );
+}
--- lib/pages/SplashScreen.dart
+++ lib/pages/SplashScreen.dart
@@ -1,13 +1,10 @@
 import 'dart:async';
 
-import 'package:clickandgoapp/Main.dart';
-import 'package:clickandgoapp/controllers/MainBottonNavController.dart';
-import 'package:clickandgoapp/pages/PaymentsPage.dart';
+import 'package:clickandgoapp/pages/AuthPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class SplashScreen extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return SplashScreenState();
@@ -15,7 +12,6 @@ class SplashScreen extends StatefulWidget {
 }
 
 class SplashScreenState extends State<SplashScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -25,42 +21,38 @@ class SplashScreenState extends State<SplashScreen> {
 
   Future performAction() {
     return Future.delayed(const Duration(seconds: 2), () {
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomeBottomNavigationController()));
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (context) => AuthPage()));
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/splash_background.jpg'),
-          fit: BoxFit.fill
-        ) ,
-      ),
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 3,
-            child: Center(
-            child: Container(
-              margin: EdgeInsets.all(64),
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/splash_icon.png'),
-                  fit: BoxFit.scaleDown
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/splash_background.jpg'),
+              fit: BoxFit.fill),
+        ),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: Center(
+                  child: Container(
+                margin: EdgeInsets.all(64),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/splash_icon.png'),
+                      fit: BoxFit.scaleDown),
                 ),
-              ),
+              )),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(),
             )
-          ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(),
-          )
-        ],
-      )
-    );
+          ],
+        ));
   }
 }
